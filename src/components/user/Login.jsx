c2cd42decdf0dd25b345cd5628eddc1b5cdfc9fb
MichaelCuadros <eclipselunar20182018@gmail.com>
2023-08-08 04:40:22 -0500

    asf

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -39,9 +39,9 @@ export const Login = () => {
 
     return (
         <>
-         
+            <header className="content__header">
                 <h1 className="content__title">Login</h1>
-            
+            </header>
 
             <div className="content__posts">
                 <form className='form-login' onSubmit={e => Login(e)}>
